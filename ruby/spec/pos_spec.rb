75b8f87ba896d8b0a8b1d8ec5a2b8fb2bd5c1481
jaysonesmith <jaysonesmith@gmail.com>
2018-07-18 22:34:35 -0700

    Ruby setup and first failing test

---
+++ ruby/spec/pos_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './pos'
+
+describe Pos do
+    describe '.barcode' do
+        it 'returns a price when a matching barcode is found' do
+            pos = Pos.new()
+            pos.barcode("1234567890")
+
+            expect(pos.last_message).to eq "$1.00"
+        end
+    end
+end
