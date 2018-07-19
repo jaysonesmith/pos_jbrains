6996376a3e6c831383b262e52562f902bde720a4
jaysonesmith <jaysonesmith@gmail.com>
2018-07-18 22:50:33 -0700

    We now look up prices in the product_store

--- ruby/pos.rb
+++ ruby/pos.rb
@@ -3,9 +3,10 @@ class Pos
 
     def initialize
         @message = ''
+        @product_store = { '1234567890' => '$1.00' }
     end
 
     def barcode(barcode_input)
-        @message = '$1.00'
+        @message = @product_store[barcode_input]
     end
 end
